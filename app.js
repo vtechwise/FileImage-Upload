aea43777c2e63468dbb76f79c606d30af7b87573
victor <oguntayovictor65@gmail.com>
2025-02-18 16:45:37 +0100

    add static controller function

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
+const productRouter = require('./routes/productRoutes')
 
 // database
 const connectDB = require('./db/connect');
 
+
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('<h1>File Upload Starter</h1>');
 });
+
+app.use('/api/v1/products', productRouter)
 
 // middleware
 app.use(notFoundMiddleware);
---
+++ controllers/uploadsController.js
@@ -0,0 +1,5 @@
+const uploadImage = (req,res) => {
+    res.send('upload image')
+}
+
+module.exports = uploadImage
